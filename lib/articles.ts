24f4308a58fc272aa55d1743a05346e34e0f7c7e
Lylekepkey <130062475+Lylekepkey@users.noreply.github.com>
2024-09-20 00:40:06 +0200

    Update articles.ts
    Error message for vercel

--- lib/articles.ts
+++ lib/articles.ts
@@ -6,7 +6,6 @@ import {remark} from "remark";
 import html from "remark-html";
 
 import type { ArticleItem } from "@/types";
-import { ArticleItem } from '../types/index';
 
 const articlesDirectory = path.join(process.cwd(), "articles");
 
